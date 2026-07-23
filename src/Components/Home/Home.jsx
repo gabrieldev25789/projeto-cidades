d2c51aea96f6e9537e36a5442fd0689b09f60e51
Gabriel <gabrielsb1090@gmail.com>
2026-07-23 00:28:14 -0300

    feat: Estruturando home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,24 @@
+import "./Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div class="cidades-container">
+        <div class="cidade-card">
+            <h3>Nova York</h3>
+        </div>
+        <div class="cidade-card">
+            <h3>Londres</h3>
+        </div>
+        <div class="cidade-card">
+            <h3>Paris</h3>
+        </div>
+        <div class="cidade-card">
+            <h3>São Paulo</h3>
+        </div>
+        <div class="cidade-card">
+            <h3>Berlim</h3>
+        </div>
+    </div>
   )
 }
 
