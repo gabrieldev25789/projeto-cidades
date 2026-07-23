f1fba5087db74ac46418e6a35370daae76c15168
Gabriel <gabrielsb1090@gmail.com>
2026-07-23 00:40:41 -0300

    feat: Estruturando header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">
+          <span className="logo-icon">🌍</span>
+          <span className="logo-text">Explorador de Cidades</span>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
