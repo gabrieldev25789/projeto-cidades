1e656c9cbeecc735c7d0a4b26bb5b31debffcf3d
Gabriel <gabrielsb1090@gmail.com>
2026-07-23 23:57:46 -0300

    feat: Adicionando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './Components/Header/Header'
 import Home from './Components/Home/Home'
+import CidadeDetalhes from './Components/CidadeDetalhes/CidadeDetalhes'
 
 function App() {
 
   return (
     <>
-    <Header />
-     <Home />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cidade/:id" element={<CidadeDetalhes />} />
+      </Routes>
     </>
   )
 }
