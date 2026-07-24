bacf34b4ffa1326a0e88604c519742ce28da8c23
Gabriel <gabrielsb1090@gmail.com>
2026-07-24 03:01:51 -0300

    feat: Removendo header do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import Header from './Components/Header/Header'
 import Home from './Components/Home/Home'
 import CidadeDetalhes from './Components/CidadeDetalhes/CidadeDetalhes'
 
@@ -8,7 +7,6 @@ function App() {
 
   return (
     <>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cidade/:id" element={<CidadeDetalhes />} />
