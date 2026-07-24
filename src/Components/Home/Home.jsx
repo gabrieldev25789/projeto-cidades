8d2a95a4f20b8b90d93c44828c66be79e9478d18
Gabriel <gabrielsb1090@gmail.com>
2026-07-24 04:06:55 -0300

    feat: Exibindo cidades com base no filtro da busca

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
+// Home.jsx
 import { useState, useEffect } from 'react'
 import "./Home.css"
 import Header from '../Header/Header.jsx'
 import cidades from "../../../data/data.js"
 import RelogioCidade from '../Relogio/Relogio.jsx'
 import { Link } from 'react-router-dom';
+import Busca from "../Busca/Busca.jsx"
 
 function getHoraCidade(timezone) {
   return new Intl.DateTimeFormat('pt-BR', {
@@ -13,7 +15,7 @@ function getHoraCidade(timezone) {
   }).format(new Date())
 }
 
-function Home() {
+function Home({ filtrado, setFiltrado }) {
   const [hora, setHora] = useState(new Date())
   console.log(hora)
 
@@ -22,43 +24,51 @@ function Home() {
     return () => clearInterval(intervalo)
   }, [])
 
+  const cidadesExibidas = filtrado ?? cidades
+
   return (
     <>
       <Header />
-    <div className="cidades-container">
-      {cidades.map((cidade) => (
-        <Link to={`/cidade/${cidade.id}`} className="cidade-card" key={cidade.id}>
-            <img src={cidade.imagem} alt={cidade.nome} />
-            <div className="cidade-hora">
-                <span>Horário local</span>
-                <RelogioCidade timezone={cidade.timezone} />
-            </div>
-            <span className="cidade-hora">{getHoraCidade(cidade.timezone)}</span>
+      <Busca setFiltrado={setFiltrado} />
 
-            <div className="cidade-conteudo">
-                <h3>{cidade.nome}</h3>
-                <p className="cidade-pais">{cidade.pais}</p>
-
-                <div className="cidade-secao">
-                    <span className="secao-titulo">Pontos turísticos</span>
-                    <ul>
-                        <li>{cidade.pontosTuristicos[0]}</li>
-                        <li>{cidade.pontosTuristicos[1]}</li>
-                        <li>{cidade.pontosTuristicos[2]}</li>
-                    </ul>
+      <div className="cidades-container">
+        {cidadesExibidas.length === 0 ? (
+          <p>Nenhuma cidade encontrada</p>
+        ) : (
+          cidadesExibidas.map((cidade) => (
+            <Link to={`/cidade/${cidade.id}`} className="cidade-card" key={cidade.id}>
+                <img src={cidade.imagem} alt={cidade.nome} />
+                <div className="cidade-hora">
+                    <span>Horário local</span>
+                    <RelogioCidade timezone={cidade.timezone} />
                 </div>
+                <span className="cidade-hora">{getHoraCidade(cidade.timezone)}</span>
 
-                <div className="cidade-secao">
-                    <span className="secao-titulo">Curiosidades</span>
-                    <ul className="curiosidades">
-                        <li>{cidade.curiosidades[0]}</li>
-                        <li>{cidade.curiosidades[1]}</li>
-                    </ul>
+                <div className="cidade-conteudo">
+                    <h3>{cidade.nome}</h3>
+                    <p className="cidade-pais">{cidade.pais}</p>
+
+                    <div className="cidade-secao">
+                        <span className="secao-titulo">Pontos turísticos</span>
+                        <ul>
+                            <li>{cidade.pontosTuristicos[0]}</li>
+                            <li>{cidade.pontosTuristicos[1]}</li>
+                            <li>{cidade.pontosTuristicos[2]}</li>
+                        </ul>
+                    </div>
+
+                    <div className="cidade-secao">
+                        <span className="secao-titulo">Curiosidades</span>
+                        <ul className="curiosidades">
+                            <li>{cidade.curiosidades[0]}</li>
+                            <li>{cidade.curiosidades[1]}</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </Link>
-        ))}
-        </div>
+            </Link>
+          ))
+        )}
+      </div>
     </>
   )
 }
