d768fcb2324473445896b41538686f107c73ea88
Gabriel <gabrielsb1090@gmail.com>
2026-07-24 04:07:34 -0300

    feat: Criando estado de filtro / passando como props

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './Components/Home/Home'
 import CidadeDetalhes from './Components/CidadeDetalhes/CidadeDetalhes'
+import { useState } from 'react'
 
 function App() {
+  
+  const [filtrado, setFiltrado] = useState(null)
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home filtrado={filtrado} setFiltrado={setFiltrado}/>} />
         <Route path="/cidade/:id" element={<CidadeDetalhes />} />
       </Routes>
     </>
