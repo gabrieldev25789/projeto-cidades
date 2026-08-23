4489dd30722ae6f239fe6dd148dcf17995a65216
Gabriel <gabrielsb1090@gmail.com>
2026-08-23 01:46:35 -0300

    feat: Passando params

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,17 @@ import { useState } from 'react'
 function App() {
   
   const [filtrado, setFiltrado] = useState(null)
+  const [filtradoContinente, setFiltradoContinente] = useState(null)
 
   return (
     <>
     <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Home filtrado={filtrado} setFiltrado={setFiltrado}/>} />
+        <Route path="/" element={<Home 
+        filtrado={filtrado} 
+        setFiltrado={setFiltrado}
+        filtradoContinente={filtradoContinente} 
+        setFiltradoContinente={setFiltradoContinente}/>} />
         <Route path="/cidade/:id" element={<CidadeDetalhes />} />
       </Routes>
     </>
